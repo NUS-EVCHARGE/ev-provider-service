b48bee4d3b4ddeb0ca74ea153bdecaeb86fd9e9e
Terry Chia <chiawz@hotmail.com>
2023-09-23 21:00:35 +0800

    fix: get all from column "user_email" instead of "user"

--- dao/db_provider.go
+++ dao/db_provider.go
@@ -13,12 +13,12 @@ func (d *dbImpl) UpdateProviderEntry(Provider dto.Provider) error {
 	return results.Error
 }
 
-func (d*dbImpl) CreateProviderEntry(Provider dto.Provider) (dto.Provider, error) {
+func (d *dbImpl) CreateProviderEntry(Provider dto.Provider) (dto.Provider, error) {
 	result := d.DbController.Create(&Provider)
 	return Provider, result.Error
 }
 
-func (d*dbImpl) DeleteProviderEntry(Provider dto.Provider) error {
+func (d *dbImpl) DeleteProviderEntry(Provider dto.Provider) error {
 	results := d.DbController.Delete(&Provider)
 	if results.RowsAffected == 0 {
 		return fmt.Errorf("Provider not found")
@@ -26,10 +26,9 @@ func (d*dbImpl) DeleteProviderEntry(Provider dto.Provider) error {
 	return results.Error
 }
 
-func (d*dbImpl) GetAllProviderEntry(email string) (dto.Provider, error) {
+func (d *dbImpl) GetAllProviderEntry(email string) (dto.Provider, error) {
 	var existingProvider dto.Provider
 
-	results := d.DbController.First(&existingProvider, "email = ?", email)
+	results := d.DbController.First(&existingProvider, "user_email = ?", email)
 	return existingProvider, results.Error
 }
-
